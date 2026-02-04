6f763e37eda8c424c3c7d8534cce907aede6e6d5
GrouchyDodaoOT <chanpasong.thipphakhinkeo@gmail.com>
2026-02-04 14:25:28 -0500

    Practice with OOP, factory function, constructor function, and class object intro

--- js/index.js
+++ js/index.js
@@ -68,3 +68,80 @@ const pContainer = document.querySelector("#jokeContainer");
 btn.addEventListener("click", async () => {
   pContainer.innerText = await getDadJoke();
 });
+
+// Factory Functions
+
+function hex(r, g, b) {
+  return `# ${((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)}`;
+}
+
+function makeColor(r, g, b) {
+  const color = {};
+  color.r = r;
+  color.g = g;
+  color.b = b;
+  color.rgb = function () {
+    const { r, g, b } = this;
+    return `rgb(${r}, ${g}, ${b})`;
+  };
+  color.hex = function () {
+    const { r, g, b } = this;
+    return `#${((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)}`;
+  };
+  return color;
+}
+
+const aColor = makeColor(0, 0, 255);
+
+// Constructor Functions
+
+// function Color(r, g, b) {
+//   this.r = r;
+//   this.g = g;
+//   this.b = b;
+// }
+
+// Color.prototype.rgb = function () {
+//   const { r, g, b } = this;
+//   return `rgb(${r}, ${g}, ${b})`;
+// };
+
+// Color.prototype.hex = function () {
+//   const { r, g, b } = this;
+//   return `#${((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)}`;
+// };
+
+// Color.prototype.rgba = function (a = 1.0) {
+//   const { r, g, b } = this;
+//   return `rgb(${r}, ${g}, ${b}, ${a})`;
+// };
+
+// const color1 = new Color(45, 245, 80);
+// const color2 = new Color(0, 0, 0);
+
+// JS Class Constructor
+
+class Color {
+  constructor(r, g, b, name) {
+    this.r = r;
+    this.b = b;
+    this.g = g;
+    this.name = name;
+  }
+  innerRGB() {
+    const { r, g, b } = this;
+    return `${r}, ${g}, ${b}`;
+  }
+  rgb() {
+    return `rgb(${this.innerRGB()})`;
+  }
+  hex() {
+    const { r, g, b } = this;
+    return `#${((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)}`;
+  }
+  rgba(a = 1.0) {
+    return `${this.innerRGB()}, ${a}`;
+  }
+}
+
+const c1 = new Color(120, 250, 240, "boogie");
